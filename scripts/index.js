7a39f73ee321f81cf52500191bd47b9fb9b31b70
Rutkovskaya <79737751+Rutkovskaya@users.noreply.github.com>
2021-04-19 15:33:34 +0300

    1

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,17 @@
 let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.profile-info__edit-button');
 let closeButton = document.querySelector('.popup__close-btn');
-let submitButton = document.querySelector('.popup__submit-btn');
+let nameProfile = document.querySelector('.profile-info__name');
+let jobProfile = document.querySelector('.profile-info__status');
+let nameInput = document.querySelector('.popup__text_type_name');
+let jobInput = document.querySelector('.popup__text_type_status');
+let formElement = document.querySelector('.popup__container');
+
 
 function openPopup() {
     popup.classList.add('popup_opened');
+    nameInput.setAttribute('value', nameProfile.textContent);
+    jobInput.setAttribute('value', jobProfile.textContent);
 }
 
 editButton.addEventListener('click', openPopup);
@@ -15,16 +22,6 @@ function closePopup() {
 
 closeButton.addEventListener('click', closePopup);
 
-let nameProfile = document.querySelector('.profile-info__name');
-let jobProfile = document.querySelector('.profile-info__status');
-let nameInput = document.querySelector('.popup__text_type_name');
-let jobInput = document.querySelector('.popup__text_type_status');
-
-nameInput.setAttribute('value', nameProfile.textContent);
-jobInput.setAttribute('value', jobProfile.textContent);
-
-let formElement = document.querySelector('.popup__container');
-
 function formSubmitHandler(evt) {
     evt.preventDefault();
 
@@ -34,8 +31,4 @@ function formSubmitHandler(evt) {
     closePopup()
 }
 
-popup.addEventListener('submit', formSubmitHandler);
-
-
-
-
+formElement.addEventListener('submit', formSubmitHandler);
