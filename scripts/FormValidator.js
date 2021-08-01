507557959dbc60b2196c3d04c91e90e07c5f8274
Rutkovskaya <79737751+Rutkovskaya@users.noreply.github.com>
2021-08-01 16:15:39 +0300

    1

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -11,7 +11,52 @@ class FormValidator {
         this._inputNotValidClass = selector.inputNotValidClass;
     }
 
+    //Показывалка ошибок
+   _showInputError = (inputElement, errorMassage, _sectionClass, _inputErrorClass, _errorClass, _inputNotValidClass) => {
+        const formSectionElement = inputElement.closest(this._sectionClass);
+        const errorElement = formSectionElement.querySelector(this._inputErrorClass);
+        errorElement.textContent = errorMassage;
+        errorElement.classList.add(this._errorClass);
+        inputElement.classList.add(this._inputNotValidClass);
+     }
+
+    //Скрывалка ошибок
+    _hideInputError = (inputElement, _sectionClass, _inputErrorClass, _errorClass, _inputNotValidClass) => {
+        const formSectionElement = inputElement.closest(this._sectionClass);
+        const errorElement = formSectionElement.querySelector(this._inputErrorClass);
+        errorElement.textContent = "";
+        errorElement.classList.add(this._errorClass);
+        inputElement.classList.remove(this._inputNotValidClass);
+    }
+
+    //Определятор запуска функции показывать или нет ошибку
+    _checkInputValidity = (_inputElement, _inputNotValidClass, _sectionClass, _inputErrorClass, _errorClass) => {
+        this._isInputNotValid = !_inputElement.validity.valid;
+
+        if (this._isInputNotValid) {
+            this._errorMassage = _inputElement.validationMessage;
+            this._showInputError(this._inputElement, this._errorMassage, this._sectionClass, this._inputErrorClass, this._errorClass, this._inputNotValidClass);
+        } else {
+            this._hideInputError(this._inputElement, this._sectionClass, this._inputErrorClass, this._errorClass, this._inputNotValidClass);
+        }
+    }
+
+    //Переключаем состояние кнопки
+    _toggleButtonState = (_inputList, _buttonElement, _inactiveButtonClass) => {
+        _findAtLeastOneNotValid = (_inputElement) => !this._inputElement.validity.valid;
+        this._hasNotValidInput = inputList.some(this._findAtLeastOneNotValid);
+
+        if (_hasNotValidInput) {
+            this._buttonElement.setAttribute("disabled", true);
+            this._buttonElement.classList.add(this._inactiveButtonClass);
+        } else {
+            this._buttonElement.removeAttribute("disabled");
+            this._buttonElement.classList.remove(this._inactiveButtonClass);
+        }
+    };
+
     enableValidation = (_formElement, _inputSelector, _submitButtonSelector, _inactiveButtonClass, _inputErrorClass, _errorClass, _sectionClass, _inputNotValidClass) => {
+        //Отменяем стандартную отправку по субмиту
         this._formElement.addEventListener("submit", (event) => {
             event.preventDefault();
         });
@@ -30,50 +75,6 @@ class FormValidator {
             })
         })
     }
-
-    //Определятор запуска функции показывать или нет ошибку
-    _checkInputValidity = (_inputElement, _inputNotValidClass, _sectionClass, _inputErrorClass, _errorClass) => {
-        this._isInputNotValid = !_inputElement.validity.valid;
-
-        if (this._isInputNotValid) {
-            this._errorMassage = _inputElement.validationMessage;
-            this._showInputError(this._inputElement, this._errorMassage, this._sectionClass, this._inputErrorClass, this._errorClass, this._inputNotValidClass);
-        } else {
-            this._hideInputError(this._inputElement, this._sectionClass, this._inputErrorClass, this._errorClass, this._inputNotValidClass);
-        }
-    }
-
-    //Показывалка ошибок
-    _showInputError = (_inputElement, _errorMassage, _sectionClass, _inputErrorClass, _errorClass, _inputNotValidClass) => {
-        this._formSectionElement = _inputElement.closest(this._sectionClass);
-        this._errorElement = _formSectionElement.querySelector(this._inputErrorClass);
-        this._errorElement.textContent = _errorMassage;
-        this._errorElement.classList.add(this._errorClass);
-        this._inputElement.classList.add(this._inputNotValidClass);
-    }
-
-    //Скрывалка ошибок
-    _hideInputError = (_inputElement, _sectionClass, _inputErrorClass, _errorClass, _inputNotValidClass) => {
-        this._formSectionElement = _inputElement.closest(this._sectionClass);
-        this._errorElement = _formSectionElement.querySelector(this._inputErrorClass);
-        this._errorElement.textContent = "";
-        this._errorElement.classList.add(this._errorClass);
-        this._inputElement.classList.remove(this._inputNotValidClass);
-    }
-
-    //Переключаем состояние кнопки
-    _toggleButtonState = (_inputList, _buttonElement, _inactiveButtonClass) => {
-        _findAtLeastOneNotValid = (_inputElement) => !this._inputElement.validity.valid;
-        this._hasNotValidInput = inputList.some(this._findAtLeastOneNotValid);
-
-        if (_hasNotValidInput) {
-            this._buttonElement.setAttribute("disabled", true);
-            this._buttonElement.classList.add(this._inactiveButtonClass);
-        } else {
-            this._buttonElement.removeAttribute("disabled");
-            this._buttonElement.classList.remove(this._inactiveButtonClass);
-        }
-    };
 
 }
 
