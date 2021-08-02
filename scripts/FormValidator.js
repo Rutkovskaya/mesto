a745a46e14551fdd77f9402acee60ea2f5d98ef0
Rutkovskaya <79737751+Rutkovskaya@users.noreply.github.com>
2021-08-02 09:19:23 +0300

    1

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -12,40 +12,37 @@ class FormValidator {
     }
 
     //Показывалка ошибок
-    _showInputError = (_inputElement, _errorMassage, _sectionClass, _inputErrorClass, _errorClass, _inputNotValidClass) => {
-        console.log(_sectionClass)
-        console.log(_inputElement)
-        const _formSectionElement = _inputElement.closest(_sectionClass);
+    _showInputError = (inputElement, _errorMassage) => {
+        const _formSectionElement = this.inputElement.closest(_sectionClass);
         const _errorElement = _formSectionElement.querySelector(_inputErrorClass);
         _errorElement.textContent = _errorMassage;
         _errorElement.classList.add(this._errorClass);
-        _inputElement.classList.add(this._inputNotValidClass);
+        inputElement.classList.add(this._inputNotValidClass);
     }
 
     //Скрывалка ошибок
-    _hideInputError = (_inputElement, _sectionClass, _inputErrorClass, _errorClass, _inputNotValidClass) => {
-        const _formSectionElement = _inputElement.closest(this._sectionClass);
+    _hideInputError = (inputElement) => {
+        const _formSectionElement = this.inputElement.closest(this._sectionClass);
         const _errorElement = _formSectionElement.querySelector(this._inputErrorClass);
         _errorElement.textContent = "";
         _errorElement.classList.add(this._errorClass);
-        _inputElement.classList.remove(this._inputNotValidClass);
+        inputElement.classList.remove(this._inputNotValidClass);
     }
 
     //Определятор запуска функции показывать или нет ошибку
-    _checkInputValidity = (_inputElement, _inputNotValidClass, _sectionClass, _inputErrorClass, _errorClass) => {
-        this._isInputNotValid = !_inputElement.validity.valid;
+    _checkInputValidity = (inputElement) => {
+        this._isInputNotValid = !inputElement.validity.valid;
         if (this._isInputNotValid) {
-            console.log(_inputElement)
-            this._errorMassage = _inputElement.validationMessage;
-            this._showInputError(this._inputElement, this._errorMassage, this._sectionClass, this._inputErrorClass, this._errorClass, this._inputNotValidClass);
+            this._errorMassage = inputElement.validationMessage;
+            this._showInputError();
         } else {
-            this._hideInputError(this._inputElement, this._sectionClass, this._inputErrorClass, this._errorClass, this._inputNotValidClass);
+            this._hideInputError();
         }
     }
 
     //Переключаем состояние кнопки
-    _toggleButtonState = (_inputList, _buttonElement, _inactiveButtonClass) => {
-        _findAtLeastOneNotValid = (_inputElement) => !this._inputElement.validity.valid;
+    _toggleButtonState() {
+        _findAtLeastOneNotValid = () => !this.inputElement.validity.valid;
         this._hasNotValidInput = inputList.some(this._findAtLeastOneNotValid);
 
         if (_hasNotValidInput) {
@@ -57,26 +54,31 @@ class FormValidator {
         }
     };
 
-    enableValidation = (_formElement, _inputSelector, _submitButtonSelector, _inactiveButtonClass, _inputErrorClass, _errorClass, _sectionClass, _inputNotValidClass) => {
+    enableValidation() {
         //Отменяем стандартную отправку по субмиту
         this._formElement.addEventListener("submit", (event) => {
             event.preventDefault();
         });
 
+        this._setEventListeners()
+    }
+
+    _setEventListeners() {
         //Получаем все поля формы
         this._inputList = Array.from(this._formElement.querySelectorAll(this._inputSelector));
         //Ищем кнопку
         this._buttonElement = this._formElement.querySelector(this._submitButtonSelector);
-        
-        //При каждом нажатии кнопки на клаве запускаем проверку ошибки
-        this._inputList.forEach(_inputElement => {
-            _inputElement.addEventListener('input', (event) => {
-                this._checkInputValidity(_inputElement, _inputNotValidClass, _sectionClass, _inputErrorClass, _errorClass);
 
-                this._toggleButtonState(_inputList, _buttonElement, _inactiveButtonClass);
+        //При каждом нажатии кнопки на клаве запускаем проверку ошибки
+        this._inputList.forEach(inputElement => {
+            inputElement.addEventListener('input', (event) => {
+                this._checkInputValidity(inputElement);
+
+                this._toggleButtonState(_inputList, _buttonElement);
             })
         })
     }
 }
+
 
 export { FormValidator }
